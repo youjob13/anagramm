e506f90a65fc788b6762a139c925fedb3597dcba
Danil Rodionov <Danil_Rodionov@epam.com>
2022-11-27 00:02:32 +0400

    test 3

--- anagramm/src/app/app.component.ts
+++ anagramm/src/app/app.component.ts
@@ -9,6 +9,6 @@ export class AppComponent implements OnInit {
   public title = 'Anagramm';
 
   ngOnInit(): void {
-    setTimeout(() => (this.title = 'Anagramm title has been changed'), 3000);
+    setTimeout(() => (this.title = 'Anagramm title has been changed!'), 3000);
   }
 }
