65184d855c7478139c4a695925fd1449b9a3fbfe
Danil Rodionov <Danil_Rodionov@epam.com>
2022-11-27 00:02:10 +0400

    test 2

--- anagramm/src/app/app.component.ts
+++ anagramm/src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'Anagramm';
+export class AppComponent implements OnInit {
+  public title = 'Anagramm';
+
+  ngOnInit(): void {
+    setTimeout(() => (this.title = 'Anagramm title has been changed'), 3000);
+  }
 }
